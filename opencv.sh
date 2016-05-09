300a37e6778dd1fe0703f16ccfd32636280f2593
Zhao.Kai <zeakey@126.com>
2016-05-09 09:27:41 +0800

    opencv

--- opencv.sh
+++ opencv.sh
@@ -21,10 +21,10 @@ fi
 cd build
 cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=../install ..
 echo 'install opencv in /runtime/opencv/install/'
-
-echo 'export PYTHONPATH=$PYTHONPATH:/runtime/opencv/install/lib/python2.7/dist-packages/' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/runtime/opencv/install/lib/' >> ~/.bashrc
-echo 'export C_INCLUDE_PATH=$C_INCLUDE_PATH:/runtime/opencv/install/include' >> ~/.bashrc
-echo 'export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/runtime/opencv/install/include' >> ~/.bashrc
-echo 'export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/runtime/opencv/install/lib/pkgconfig' >> ~/.bashrc
+echo '#opencv' >> ~/.bashrc
+echo 'export PYTHONPATH=/runtime/opencv/install/lib/python2.7/dist-packages:$PYTHONPATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/runtime/opencv/install/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export C_INCLUDE_PATH=/runtime/opencv/install/include:$C_INCLUDE_PATH' >> ~/.bashrc
+echo 'export CPLUS_INCLUDE_PATH=/runtime/opencv/install/include:$CPLUS_INCLUDE_PATH' >> ~/.bashrc
+echo 'export PKG_CONFIG_PATH=/runtime/opencv/install/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
 source ~/.bashrc
